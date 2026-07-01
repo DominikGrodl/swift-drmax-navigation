60c430bdb47ae4835070e4e54b7cc1630b065d02
Dominik Grodl <dominikgrodl@icloud.com>
2026-07-01 13:43:40 +0200

    CaseEquatable (#1)
    * Introduce CaseEquatable
    
    Introduce CaseEquatable and the appropriate navigate overload
    
    * Include the animation flag when popping back
    
    * Add tests

---
+++ Sources/DrMaxNavigation/RootNavigationController/RootNavigationController+CaseEquatable.swift
@@ -0,0 +1,20 @@
+extension RootNavigationController where Screen: CaseEquatable {
+    public func navigate(
+        to screen: Screen,
+        style: NavigationStyle = .push,
+        animated: Bool = true,
+        allowsSameScreenNesting: Bool = true,
+        completion: @escaping () -> Void = {}
+    ) {
+        if !allowsSameScreenNesting, let first = completePath.first(where: screen.equals) {
+            popBefore(first, animated: animated)
+        }
+        
+        navigate(
+            to: screen,
+            style: style,
+            animated: animated,
+            completion: completion
+        )
+    }
+}
